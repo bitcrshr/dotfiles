ae5a644d1e4a2c5783c054033efae0f6c9dc4c58
Chandler Davis <chandler.davis99@icloud.com>
2025-06-01 10:15:34 -0400

    set up flake for extending

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bitcrshr's many machines";
 
   inputs = {
-     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,41 +17,44 @@
   };
 
   outputs = { nixpkgs, home-manager, disko, nixos-facter-modules, ... }:
-  let
-    mkPkgs = system: import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
+    let
+      mkPkgs = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-    mkHomeConfig = { system, username, homeDirectory, modules }:
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = mkPkgs system;
-        modules = [
-          {
-            nixpkgs = {
-              config = {
-                allowUnfree = true;
+      mkHomeConfig = { system, username, homeDirectory, modules }:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = mkPkgs system;
+          modules = [
+            {
+              nixpkgs = {
+                config = {
+                  allowUnfree = true;
+                };
               };
-            };
-            home.username = username;
-            home.homeDirectory = homeDirectory;
-            home.stateVersion = "25.05";
-            programs.home-manager.enable = true;
-          }
-        ] ++ modules;
-      };
-  in
-  {
-    homeConfigurations = {
-      "laptop" = mkHomeConfig {
-        system = "aarch64-darwin";
-        username = "chandler";
-        homeDirectory = "/Users/chandler";
-        modules = [
-          (import ./hosts/laptop).homeConfiguration
-        ];
+              home.username = username;
+              home.homeDirectory = homeDirectory;
+              home.stateVersion = "25.05";
+              programs.home-manager.enable = true;
+            }
+          ] ++ modules;
+        };
+    in
+    {
+      homeConfigurations = {
+        "laptop" = mkHomeConfig {
+          system = "aarch64-darwin";
+          username = "chandler";
+          homeDirectory = "/Users/chandler";
+          modules = [
+            (import ./hosts/laptop).homeConfiguration
+          ];
+        };
       };
 
+      # Outputs for other flakes to extend this one are below
+
+      baseModules = import ./modules;
     };
-  };
 }
