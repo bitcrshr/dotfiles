f669d1fe784d5f7de51c1b55f995d3ad545aedab
Chandler Davis <chandler@aivainc.com>
2025-06-26 20:36:51 +0000

    double oops

--- modules/hm/helix/languages.nix
+++ modules/hm/helix/languages.nix
@@ -14,66 +14,66 @@
         };
 
       };
-
-      language = [
-        {
-          name = "go";
-          auto-format = true;
-          language-servers = [ "gopls" "golangci-lint-lsp" ];
-        }
-
-        {
-          name = "rust";
-          auto-format = true;
-        }
-
-        {
-          name = "json";
-          formatter = {
-            command = "prettier";
-            args = [ "--parser" "json" ];
-          };
-          auto-format = true;
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-        }
-
-        {
-          name = "html";
-          formatter = {
-            command = "prettier";
-            args = [ "--parser" "html" ];
-          };
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-        }
-
-        {
-          name = "svelte";
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-          auto-format = true;
-          language-servers = [ "svelteserver" "tailwindcss-ls" ];
-        }
-
-        {
-          name = "nix";
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-          auto-format = true;
-          formatter = {
-            command = "nixpkgs-fmt";
-          };
-        }
-      ];
     };
+
+    language = [
+      {
+        name = "go";
+        auto-format = true;
+        language-servers = [ "gopls" "golangci-lint-lsp" ];
+      }
+
+      {
+        name = "rust";
+        auto-format = true;
+      }
+
+      {
+        name = "json";
+        formatter = {
+          command = "prettier";
+          args = [ "--parser" "json" ];
+        };
+        auto-format = true;
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+      }
+
+      {
+        name = "html";
+        formatter = {
+          command = "prettier";
+          args = [ "--parser" "html" ];
+        };
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+      }
+
+      {
+        name = "svelte";
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+        auto-format = true;
+        language-servers = [ "svelteserver" "tailwindcss-ls" ];
+      }
+
+      {
+        name = "nix";
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+        auto-format = true;
+        formatter = {
+          command = "nixpkgs-fmt";
+        };
+      }
+    ];
   };
 }
