354308a1ed2da8a509bd5ebc023bf942b53c47d4
Chandler Davis <chandler@aivainc.com>
2025-06-26 20:32:24 +0000

    fix golangci-lint lsp

--- modules/hm/helix/languages.nix
+++ modules/hm/helix/languages.nix
@@ -1,63 +1,78 @@
 { pkgs, ... }: {
   programs.helix.languages = {
-    language-server.gopls = {
-      goimports = true;
-      gofumpt = true;
-      staticcheck = false;
+    language-server = {
+      gopls = {
+        goimports = true;
+        gofumpt = true;
+        staticcheck = true;
+      };
+
+      golangci-lint-lsp = {
+        command = "golangci-lint-langserver";
+        config = {
+          command = [ "golangci-lint" "run" "--output.json.path" "stdout" "--show-status=false" "--issues-exit-code=1" ];
+        };
+
+      };
+
+      language = [
+        {
+          name = "go";
+          auto-format = true;
+          language-servers = [ "gopls" "golangci-lint-lsp" ];
+        }
+
+        {
+          name = "rust";
+          auto-format = true;
+        }
+
+        {
+          name = "json";
+          formatter = {
+            command = "prettier";
+            args = [ "--parser" "json" ];
+          };
+          auto-format = true;
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+        }
+
+        {
+          name = "html";
+          formatter = {
+            command = "prettier";
+            args = [ "--parser" "html" ];
+          };
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+        }
+
+        {
+          name = "svelte";
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+          auto-format = true;
+          language-servers = [ "svelteserver" "tailwindcss-ls" ];
+        }
+
+        {
+          name = "nix";
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+          auto-format = true;
+          formatter = {
+            command = "nixpkgs-fmt";
+          };
+        }
+      ];
     };
-
-    language = [
-      {
-        name = "rust";
-        auto-format = true;
-      }
-
-      {
-        name = "json";
-        formatter = {
-          command = "prettier";
-          args = [ "--parser" "json" ];
-        };
-        auto-format = true;
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-
-      {
-        name = "html";
-        formatter = {
-          command = "prettier";
-          args = [ "--parser" "html" ];
-        };
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-
-      {
-        name = "svelte";
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-        auto-format = true;
-        language-servers = [ "svelteserver" "tailwindcss-ls" ];
-      }
-
-      {
-        name = "nix";
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-        auto-format = true;
-        formatter = {
-          command = "nixpkgs-fmt";
-        };
-      }
-    ];
-  };
-}
+  }
