86d9974f5262ed7cbb4f79aab638b59556212f54
Chandler Davis <chandler.davis99@icloud.com>
2025-07-26 17:36:32 +0000

    maybe?

--- hosts/virt/default.nix
+++ hosts/virt/default.nix
@@ -21,13 +21,10 @@
     ];
 
   # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "bitcrshr-gaming"; # Define your hostname.
+  networking.hostName = "bitcrshr-virt"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
